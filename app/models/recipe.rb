77dde4b8004ce7c4d60e7f7fe334bb32a507ca6e
Jason Leach <jlspursfan@gmail.com>
2019-06-26 23:43:13 -0700

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,1 +1,19 @@
 #Placeholder for a model
+class Recipe < ActiveRecord::Base
+
+  @@all = []
+
+  attr_accessor :name, :ingredients, :cook_time
+
+  def initialize(name, ingredients, cook_time)
+    @name = name
+    @ingredients = ingredients
+    @cook_time = cook_time
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
